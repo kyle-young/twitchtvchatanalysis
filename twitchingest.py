d717e39244f7b39d20a00c5184b785c04328361e
kyle-young <readme.py@gmail.com>
2015-08-20 16:09:40 -0700

    Added function to store usernames from irc

--- twitchingest.py
+++ twitchingest.py
@@ -6,11 +6,11 @@ import datetime
 import sqlite3
 
 # Settings
-HOST = "<host>"            # Hostname of the IRC-Server in this case twitch's
-PORT = <port>              # Default IRC-Port
-CHAN = "#<channel>"        # Channelname = #{Nickname}
-NICK = "<nick>"            # Nickname = Twitch username
-PASS = "oauth:<authkey>"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
+HOST = ""            # Hostname of the IRC-Server in this case twitch's
+PORT = 0              # Default IRC-Port
+CHAN = ""        # Channelname = #{Nickname}
+NICK = ""            # Nickname = Twitch username
+PASS = "oauth:" # www.twitchapps.com/tmi/ will help to retrieve the required authkey
 
 # gets the name of the db in the "settings" file  
 with open('settings','r') as s:
@@ -41,6 +41,10 @@ def join_channel(chan):
 def part_channel(chan):
     con.send(bytes('PART %s\r\n' % chan, 'UTF-8'))
 
+def get_user(msg):
+    msg = str(msg[0])
+    user = msg[msg.find(":")+1:msg.find("!")]
+    return user
 
 def get_message(msg):
     result = ""
@@ -68,33 +72,37 @@ db.execute('PRAGMA synchonous = OFF')
 db.execute('PRAGMA journal_mode = MEMORY')
 
 print("Opened db successfully")
+db.execute('BEGIN;');
+try:
+    while True:
+        try:
+            data = con.recv(1024).decode('UTF-8', "ignore")
+            data_split = re.split(r"[~\r\n]+", data)
 
-while True:
-    try:
-        data = con.recv(1024).decode('UTF-8', "ignore")
-        data_split = re.split(r"[~\r\n]+", data)
+            for line in data_split:
+                line = str.split(line)
+                time = datetime.datetime.now()
+                if len(line) > 1:
+                    if line[0] == 'PING':
+                        send_pong("PONG")
 
-        for line in data_split:
-            line = str.split(line)
-            time = datetime.datetime.now()
+                    if line[1] == 'PRIVMSG':
+                        current_time = time.isoformat()
+                        message = get_message(line)
+                        user = get_user(line)
+                        params = (current_time, user, message)
 
-            if len(line) > 1:
-                if line[0] == 'PING':
-                    send_pong("PONG")
+                        print(current_time + " : " + user +  " : " + message)
+                        db.execute("INSERT INTO logs (datetime, user, message) VALUES (?,?,?)", params)
 
-                if line[1] == 'PRIVMSG':
-                    current_time = time.isoformat()
-                    message = get_message(line)
-                    params = (current_time, message)
+        except socket.error:
+            print("Socket died")
 
-                    print(current_time + ": " + message)
-                    db.execute("INSERT INTO logs (datetime, message) VALUES (?, ?)", params)
-                    db.commit()
-
-    except socket.error:
-        print("Socket died")
-
-    except socket.timeout:
-        print("Socket timeout")
+        except socket.timeout:
+            print("Socket timeout")
+except KeyboardInterrupt:
+    pass
         
+db.commit()
+print("committed!")
 db.close()
